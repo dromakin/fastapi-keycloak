2c3fd4e353ca49e70ef3d84776dd5f36f986c0da
dromakin <d.romakin@npo-echelon.ru>
2022-10-12 19:20:14 +0300

    add toml and setup

--- setup.py
+++ setup.py
@@ -12,8 +12,6 @@ __copyright__ = "Echelon, Inc, 2022"
 __status__ = 'Development'
 __version__ = 20221012
 
-
-
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 
@@ -30,24 +28,47 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 
 # This call to setup() does all the work
 setup(
-    name="FastAPI Keycloak Manager",
+    name="fastapi-keycloak-manager",
     version="1.0.0",
-    description="Demo library",
+    description="fastapi-keycloak-manager is a Python package that integrate FastAPI app and Keycloak IAM system.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://fastapi-keycloak-manager.readthedocs.io/",
     author="Dmitriy Romakin",
     author_email="dvromakin@gmail.com",
     license="MIT",
+    keywords=["keycloak", "openid", "oidc", "fastapi", 'Authentication', 'Authorization', 'Keycloak', 'FastAPI'],
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Operating System :: OS Independent"
+        "Operating System :: MacOS",
+        "Operating System :: Unix",
+        "Operating System :: Microsoft :: Windows",
     ],
     packages=find_packages(include=['fastapi_keycloak_manager']),
     include_package_data=True,
-    install_requires=["numpy"]
+    install_requires=[
+        "anyio>=3.4.0",
+        "asgiref>=3.4.1",
+        "certifi>=2021.10.8",
+        "charset-normalizer>=2.0.9",
+        "click>=8.0.3",
+        "ecdsa>=0.17.0",
+        "fastapi>=0.70.1",
+        "idna>=3.3",
+        "pyasn1>=0.4.8",
+        "pydantic>=1.5a1",
+        "python-jose>=3.3.0",
+        "requests>=2.26.0",
+        "rsa>=4.8",
+        "six>=1.16.0",
+        "sniffio>=1.2.0",
+        "starlette>=0.16.0",
+        "typing_extensions>=4.0.1",
+        "urllib3>=1.26.7",
+        "uvicorn>=0.16.0",
+    ]
 )
